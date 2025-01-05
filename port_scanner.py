720fc27f494c3be104868872dec768dec6a11544
AlexSrpls <alexandros.saropoulos@edu.turkuamk.fi>
2025-01-05 20:15:07 +0200

    updated code

--- port_scanner.py
+++ port_scanner.py
@@ -5,7 +5,7 @@ def get_open_ports(target, port_range, verbose_mode=False):
     open_ports = []
     try:
         ip = socket.gethostbyname(target)
-        for port in range(port_range[0], port_range[1]):
+        for port in range(port_range[0], port_range[1] + 1):
             s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
             socket.setdefaulttimeout(1)
 
@@ -16,11 +16,33 @@ def get_open_ports(target, port_range, verbose_mode=False):
     except KeyboardInterrupt:
         return("Exiting program!!!")
     except socket.gaierror:
-        if(re.search('[a-zA-Z]'), target):
+        if(re.search('[a-zA-Z]', target)):
             return("Error: Invalid hostname")
         return("Error: Invalid IP address")
     except socket.error:
         return("Error: Invalid IP address")
+    
+    host = None
+
+    try:
+        host = socket.gethostbyaddr(ip)[0]
+    except socket.herror:
+        host = None
+    
+    final_string = " Open port for"
+    if host != None:
+        final_string += " {url} {ip}".format(url=host, ip=ip)
+    else:
+        final_string += "\n"
+        if verbose_mode:
+            header = "PORT    SERVICE\n"
+            body = ""
+            for port in open_ports:
+                body += "{p}".format(p=port) + " "*(9-len(str(port))) +  "{sn}".format(sn=socket.getservbyport(port))
+        if(open_ports[len(open_ports)-1] !=port):
+            body += "\n"
+    return final_string + header + body
+
 
     return(open_ports)
   
