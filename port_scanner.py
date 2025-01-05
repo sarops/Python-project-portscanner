957d4a76a12403dc759a54f29c885d22418352ed
AlexSrpls <alexandros.saropoulos@edu.turkuamk.fi>
2025-01-05 19:55:52 +0200

    updated code

--- port_scanner.py
+++ port_scanner.py
@@ -1,6 +1,26 @@
-import socket, sys
+import socket, re
 
 def get_open_ports(target, port_range, verbose_mode=False):
+    ip = ""
     open_ports = []
+    try:
+        ip = socket.gethostbyname(target)
+        for port in range(port_range[0], port_range[1]):
+            s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            socket.setdefaulttimeout(1)
 
+            result = s.connect_ex((ip, port))
+            if result == 0:
+                open_ports.appned(port)
+            s.close()
+    except KeyboardInterrupt:
+        return("Exiting program!!!")
+    except socket.gaierror:
+        if(re.search('[a-zA-Z]'), target):
+            return("Error: Invalid hostname")
+        return("Error: Invalid IP address")
+    except socket.error:
+        return("Error: Invalid IP address")
+
+    return(open_ports)
   
